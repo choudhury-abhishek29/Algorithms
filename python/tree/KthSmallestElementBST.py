47baa5b183a62abe7fd67b79094839e9f7e010a0
choudhury-abhishek29 <choudhury.abhishek29@gmail.com>
2024-02-14 11:19:50 -0800

    KthSmallestElementBST python update

--- python/tree/KthSmallestElementBST.py
+++ python/tree/KthSmallestElementBST.py
@@ -5,11 +5,26 @@ from tree import TreeNode
 
 class KthSmallestElementBST:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        def dfs(node: Optional[TreeNode], list:[int]) -> [int]:
-            if node:
-                dfs(node.left, list)
-                list.append(node.val)
-                dfs(node.right, list)
-            return list
-        sortedList = dfs(root, [])
-        return sortedList[k-1]
+        stk = []
+        curr = root
+        n = 0
+        while stk or curr:
+            while curr:
+                stk.append(curr)
+                curr = curr.left
+            curr = stk.pop()
+
+            n += 1
+            if n == k:
+                return curr.val
+            curr = curr.right
+
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     def dfs(node: Optional[TreeNode], list:[int]) -> [int]:
+    #         if node:
+    #             dfs(node.left, list)
+    #             list.append(node.val)
+    #             dfs(node.right, list)
+    #         return list
+    #     sortedList = dfs(root, [])
+    #     return sortedList[k-1]
